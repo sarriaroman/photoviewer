4a2725b0c3ca815bf194cace5e4955d65d26f6cc
dawidfiruzek <dawid.firuzek@gmail.com>
2016-11-30 12:21:06 +0100

    Added back fit() combined with centerInside()
    Added fit() method back combined with centerInside(), because of case with huge images. When that case is present, photo is scalled down and then it will be center inside ImageView just like explained in previous commit.

--- src/android/PhotoActivity.java
+++ src/android/PhotoActivity.java
@@ -138,6 +138,7 @@ public class PhotoActivity extends Activity {
 		if( imageUrl.startsWith("http") ) {
 		Picasso.with(this)
 				.load(imageUrl)
+				.fit()
 				.centerInside()
 				.into(photo, new com.squareup.picasso.Callback() {
 					@Override
