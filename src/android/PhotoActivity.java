447b25025e9e3964307578ef8adca412335a1e38
dawidfiruzek <dawid.firuzek@gmail.com>
2016-11-30 10:50:31 +0100

    Changed adjusting photo to ImageView
    fit() photo is changed to centerInside(), because of problem with scale. When proportions of the photo are different than ImageView's, photo is distorted while using fit() method.  When we use centerInside() it is adjusted to at least one ImageView's dimension and it keeps the photo ratio.

--- src/android/PhotoActivity.java
+++ src/android/PhotoActivity.java
@@ -138,7 +138,7 @@ public class PhotoActivity extends Activity {
 		if( imageUrl.startsWith("http") ) {
 		Picasso.with(this)
 				.load(imageUrl)
-				.fit()
+				.centerInside()
 				.into(photo, new com.squareup.picasso.Callback() {
 					@Override
 					public void onSuccess() {
