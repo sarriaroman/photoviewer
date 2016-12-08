0bb15c68a7983b543b3dfa18e7985afc34f1496c
fiahfy <d.fiahfy@gmail.com>
2016-12-08 13:45:59 +0900

    Impl closedCallback

--- www/PhotoViewer.js
+++ www/PhotoViewer.js
@@ -1,13 +1,27 @@
 var exec = require('cordova/exec');
 
 exports.show = function(url, title, options) {
-    if( title == undefined ) {
-      title = '';
+    if ( title == undefined ) {
+        title = '';
     }
 
-    if(typeof options == "undefined"){
+    if (typeof options == "undefined") {
         options = {};
     }
 
-    exec(function(){}, function(){}, "PhotoViewer", "show", [url, title, options]);
+    exec(
+         function(result) {
+            var closed = result && result.closed
+            if (closed) {
+                if (typeof options.closedCallback === "function") {
+                    options.closedCallback();
+                }
+                return;
+            }
+         },
+         function() {},
+         "PhotoViewer",
+         "show",
+         [url, title, options]
+    );
 };
