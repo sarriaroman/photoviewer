d0307b8e135399411978c947d28648e4ed04a562
deepakrokz <deepak.gvu@gmail.com>
2018-03-20 15:38:44 +0530

    Update PhotoMultipleActivity.java

--- src/android/PhotoMultipleActivity.java
+++ src/android/PhotoMultipleActivity.java
@@ -22,7 +22,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
-import com.wedigtech.ananta.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
