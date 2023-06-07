5ebf1e534b745a2ea14507f17210b8e14322b8f4
Abid <abid.riaz@s2partnership.co.uk>
2023-06-07 18:01:47 +0100

    Added the right import to use Build to access device SDK version
    Tested on Tab Active 3 on Android 13
    Tested on Tab Active 3 on Android 12
    Both working correctly.

--- src/android/PhotoViewer.java
+++ src/android/PhotoViewer.java
@@ -3,6 +3,7 @@ package com.sarriaroman.PhotoViewer;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
